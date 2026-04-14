1ee7b873017f37a8887dc291fd1ae400b7b37573
RosebethMapa <rosebethmapa62@gmail.com>
2026-04-14 19:28:03 +0800

    force pymysql in wsgi for vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,13 +1,7 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 from django.core.wsgi import get_wsgi_application
 
